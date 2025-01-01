61999d56c97bad12deb528b545d52ae6bfc52d8a
vaibhav kolhe <kolhevaibhav2601@gmail.com>
2025-01-02 00:56:44 +0530

    added index.html

---
+++ neww.js
@@ -0,0 +1,49 @@
+let age = 17;
+let grace = 2;
+console.log(age + grace)
+
+if ((age + grace) > 18) {
+    console.log("you can drive")
+
+} else {
+    console.log("you cant drive")
+
+}
+
+
+{
+    let age = 6;
+    if (age == 17) {
+        console.log("learning licance")
+
+    } else if (age == 16) {
+        console.log("nabalik")
+    }
+    else if (age == 18) { console.log("can drive car,and get a car licance") }
+    else if (age > 20) { console.log("shuld have licance") }
+    else { console.log("you  are gay ") }
+
+}
+{
+    let a = 2
+    for (let b = 0; b < 10; b++) {
+        console.log(a + b);
+
+    }
+}
+{
+    let f = 1;
+    while (f < 6) {
+        console.log(f);
+        f++;
+
+    }
+}
+{
+    let e = 5
+    do {
+        console.log(e)
+        e++
+
+    } while (e <10);
+}
